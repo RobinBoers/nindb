abeeb5571b32aee2355cf5d445bd764c38346033
Robin Boers <robindev2019@outlook.com>
2021-11-21 10:54:35 +0100

    Removed 250 char limit for posts

--- lib/nin_db/schemas/post.ex
+++ lib/nin_db/schemas/post.ex
@@ -5,7 +5,7 @@ defmodule NinDB.Post do
   schema "posts" do
     field :author_id, :integer
     field :title, :string, size: 70
-    field :body, :string, size: 250
+    field :body, :string
     field :image, :binary
     field :datetime, :naive_datetime
   end
@@ -21,6 +21,5 @@ defmodule NinDB.Post do
       ])
     |> validate_required([:author_id, :title, :body, :datetime])
     |> validate_length(:title, max: 70)
-    |> validate_length(:body, max: 250)
   end
 end
