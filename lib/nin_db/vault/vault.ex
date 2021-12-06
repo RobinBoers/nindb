e3af7dd50b1bd79efee49a61aa385703069493f5
Robin Boers <robindev2019@outlook.com>
2021-12-06 19:03:33 +0100

    Use env variable in Cloak config

--- lib/nin_db/vault/vault.ex
+++ lib/nin_db/vault/vault.ex
@@ -1,3 +1,24 @@
 defmodule NinDB.Vault do
   use Cloak.Vault, otp_app: :nin_db
+
+  @impl GenServer
+  def init(config) do
+    config =
+      Keyword.put(config, :ciphers, [
+        default: {
+          Cloak.Ciphers.AES.GCM,
+          tag: "AES.GCM.V1",
+          key: decode_env!("CLOAK_KEY"),
+          iv_length: 12
+        }
+      ])
+
+    {:ok, config}
+  end
+
+  defp decode_env!(var) do
+    var
+    |> System.get_env()
+    |> Base.decode64!()
+  end
 end
