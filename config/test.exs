051debf62a969e287b2c3a604341eb71728e0df5
Robin Boers <robindev2019@outlook.com>
2021-12-06 16:54:44 +0100

    Revert "Prepare for unit tests"
    This reverts commit 9cc96e46cc6f8296684b249cf66f711f2350f7b8.

--- config/test.exs
+++
@@ -1,6 +0,0 @@
-use Mix.Config
-
-config :nin_db, NinDB.Repo,
-  database: "nindb_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
---
+++ test/nin_db_test.exs
@@ -0,0 +1,8 @@
+defmodule NinDBTest do
+  use ExUnit.Case
+  doctest NinDB
+
+  test "greets the world" do
+    assert NinDB.hello() == :world
+  end
+end
